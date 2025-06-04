e8f68e93b99dc08e1cbad031efb8b79b5a4f4c3d
Loc Dang <locdpxse182614@fpt.edu.vn>
2025-06-05 00:41:58 +0700

    DbContext fix

--- CosplayDate-BE/CosplayDate.Infrastructure/Data/Context/CosplayDateDbContext.cs
+++ CosplayDate-BE/CosplayDate.Infrastructure/Data/Context/CosplayDateDbContext.cs
@@ -72,9 +72,9 @@ public partial class CosplayDateDbContext : DbContext
 
     public virtual DbSet<WalletTransaction> WalletTransactions { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=(local);Database=CosplayDateDB;User Id=sa;Password=12345;TrustServerCertificate=true;");
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseSqlServer("Server=(local);Database=CosplayDateDB;User Id=sa;Password=12345;TrustServerCertificate=true;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
